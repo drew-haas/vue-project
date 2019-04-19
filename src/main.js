8591059a388d466965d24f22b2919edf19ac0b18
Andrew Haas <ahaas3@apple.com>
2019-04-19 13:18:42 -0700

    add projects component, add data and computed properties to main.js, change path to empty on home in main js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import App from './App.vue';
-import Home from './components/Home.vue';
-import About from './components/About.vue';
-import Locations from './components/Locations.vue';
-import Contact from './components/Contact.vue';
+import App from '@/App.vue';
+import Home from '@/components/Home.vue';
+import About from '@/components/About.vue';
+import Locations from '@/components/Locations.vue';
+import Contact from '@/components/Contact.vue';
+import Projects from '@/components/Projects.vue';
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '', component: Home },
   { path: '/about', component: About },
   { path: '/locations', component: Locations },
   { path: '/contact', component: Contact },
+  { path: '/projects/:id', name: 'Projects', component: Projects}
 ];
 
 const router = new VueRouter({
@@ -23,6 +25,14 @@ const router = new VueRouter({
 
 new Vue({
   el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
   router,
   render: h => h(App)
 }).$mount("#app");
